6b5c1f6b666eb00a203fccd07be3f002bd32da92
lucas silva <lucassilva.ferre@gmail.com>
2023-08-11 20:20:26 -0300

    Alterando nome da variavel itens em Itens

--- src/pages/Itens/index.tsx
+++ src/pages/Itens/index.tsx
@@ -1,10 +1,10 @@
 import Item from "./Item"
-import itens from "./itens.json"
+import cardapio from "./itens.json"
 import styles from "./Itens.module.scss"
 export default function Itens() {
     return(
         <div className={styles.itens}>
-        {itens.map((item) => (
+        {cardapio.map((item) => (
             <Item key={item.id}/>
         ))}
         </div>
