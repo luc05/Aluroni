4951b787e2e61e702e8b4df1119c7f3d25433cf8
lucas silva <lucassilva.ferre@gmail.com>
2023-08-14 20:13:43 -0300

    Criando o menu

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 
+import Menu from "components";
 import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -6,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default function AppRouter() {
   return (
     <Router>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/cardapio" element={<Cardapio />} />
