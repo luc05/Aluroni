4e357fcc256f4f4244eeee5d47e1bdc3c2e2599f
lucas silva <lucassilva.ferre@gmail.com>
2023-08-15 12:02:59 -0300

    Terminando a tela Sobre

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,11 +1,13 @@
 import cardapio from "assets/dados/cardapio.json";
 import styles from "./Inicio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+import nossaCasa from "assets/nossa_casa.png";
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
   return (
     <section>
-      <h3 className={styles.titulo}>
+      <h3 className={stylesTema.titulo}>
         Recomendações da cozinha
       </h3>
       <div className={styles.recomendados}>
@@ -19,6 +21,13 @@ export default function Inicio() {
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do Aluroni"></img>
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br /> <br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
